f98ddc9d0b51f516e38e93b53023d894e0786b40
Sayan Bhattacharjee <88587858+Sayan0718@users.noreply.github.com>
2023-09-08 21:28:03 +0530

    Update RandomPassword.py

--- Task 2/RandomPassword.py
+++ Task 2/RandomPassword.py
@@ -1,1 +1,26 @@
 
+import random
+import string
+
+print("""
+    HELLO USER
+WELCOME TO RANDOM PASSWORD GENERATOR""")
+
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+digits = string.digits
+special_symbols = string.punctuation
+
+combination = lower + upper + digits + special_symbols
+
+l = int(input("Enter The Length Of The Password = "))
+
+temp = random.sample(combination, l)
+
+password = "".join(temp)
+
+print("You Password Of ",l, " Size Is Created")
+print("Password = ", password)
+
+print("THANK YOU FOR USING")
+
